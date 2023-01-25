589924b0db7e58e0a03095155be52cbdf186467f
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-01-24 21:01:16 -0800

    Update jsrepair.bat

--- installers/jsrepair.bat
+++ installers/jsrepair.bat
@@ -5,8 +5,8 @@ echo _(  )(  __)(  )  (  )  ( \/ )/ ___)(  __)(  __)(  _ \(  _ \
 echo/ \) \ ) _) / (_/\/ (_/\ )  / \___ \ ) _)  ) _)  )   / )   /
 echo\____/(____)\____/\____/(__/  (____/(____)(____)(__\_)(__\_)                                                                                 
 
-cd %HOMEDRIVE%
-del jellyseerr-main
+cd C:\
+del C:\jellyseerr-main
 wget https://github.com/Fallenbagel/jellyseerr/archive/refs/heads/main.zip
 7z x "main.zip" -o%HOMEDRIVE%
 start jsinst.bat
