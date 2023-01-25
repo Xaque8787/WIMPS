16e7168a5f445b4f815b2cb69f39b422ff921920
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-01-24 21:24:49 -0800

    Update jsupdate.bat

--- installers/jsupdate.bat
+++ installers/jsupdate.bat
@@ -5,14 +5,14 @@ echo _(  )(  __)(  )  (  )  ( \/ )/ ___)(  __)(  __)(  _ \(  _ \
 echo/ \) \ ) _) / (_/\/ (_/\ )  / \___ \ ) _)  ) _)  )   / )   /
 echo\____/(____)\____/\____/(__/  (____/(____)(____)(__\_)(__\_)                                                                                 
 
-cd %HOMEDRIVE%/jellyseerr-main
+cd C:\jellyseerr-main
 xcopy /i /s /y "config" "%HOMEDRIVE%"
 wget https://github.com/Fallenbagel/jellyseerr/archive/refs/heads/main.zip
-del jellyseerr-main
+del /F /Q /S jellyseerr-main
 7z x "main.zip" -o%HOMEDRIVE%
 cd %HOMEDRIVE%/jellyseerr-main
 npm install -g win-node-env && yarn install && yarn run build
-cd %HOMEDRIVE%
+cd C:\
 xcopy /i /s /y "config" "%HOMEDRIVE%/jellyseerr-main"
 
 
