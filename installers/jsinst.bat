721976ec83840932b38f30dcbcafa5604d6e5087
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-01-24 20:34:28 -0800

    Update jsinst.bat

--- installers/jsinst.bat
+++ installers/jsinst.bat
@@ -6,6 +6,8 @@ echo/ \) \ ) _) / (_/\/ (_/\ )  / \___ \ ) _)  ) _)  )   / )   /
 echo\____/(____)\____/\____/(__/  (____/(____)(____)(__\_)(__\_)                                                                                 
 
 cd %HOMEDRIVE%/jellyseerr-main
-npm install -g npm && npm install -g win-node-env && yarn install && yarn run build
+call npm install -g npm 
+call npm install -g win-node-env 
+yarn install && yarn run build
 
 timeout 3
