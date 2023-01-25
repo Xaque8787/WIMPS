b0024dc90e4a342c041bae578637af721d6eaf1c
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-01-24 19:44:15 -0800

    Update firefox.bat

--- installers/firefox.bat
+++ installers/firefox.bat
@@ -5,7 +5,7 @@ echo(  __)(  )(  _ \(  __)(  __)/  \( \/ )
 echo ) _)  )(  )   / ) _)  ) _)(  O ))  ( 
 echo(__)  (__)(__\_)(____)(__)  \__/(_/\_)                                                                                 
 
-wget --retry-connrefused  --read-timeout=20 --timeout=15 --tries=10 --continue firefox.exe https://ftp.mozilla.org/pub/firefox/releases/109.0/win64/en-US/Firefox%20Setup%20109.0.exe
+wget --retry-connrefused  --read-timeout=20 --timeout=15 --tries=10 --continue firefox.exe https://ftp.mozilla.org/pub/firefox/releases/109.0/win64/en-US/Firefox^%20Setup^%20109.0.exe
 firefox.exe /S
 timeout 3
 del firefox.exe
