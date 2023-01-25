27857d63ff4041acfb0aadc19aad2b1efdca6ede
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-01-24 19:43:07 -0800

    Update caddy.bat

--- installers/caddy.bat
+++ installers/caddy.bat
@@ -6,7 +6,7 @@ echo / __) / _\ (    \(    \( \/ )
 echo( (__ /    \ ) D ( ) D ( )  / 
 echo \___)\_/\_/(____/(____/(__/  
                                                                                  
-wget --retry-connrefused  --read-timeout=20 --timeout=15 --tries=10 --continue -O caddy.exe https://caddyserver.com/api/download?os=windows&arch=amd64&idempotency=40655382046895
+wget --retry-connrefused  --read-timeout=20 --timeout=15 --tries=10 --continue -O caddy.exe https://caddyserver.com/api/download?os=windows^&arch=amd64^&idempotency=40655382046895
 mkdir "%HOMEDRIVE%\caddy"
 move "caddy.exe" "%HOMEDRIVE%\caddy"
 timeout 3
