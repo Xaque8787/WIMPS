caef0c3de805cacacc7680b2199afbfc24da964e
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-01-31 19:29:02 -0800

    Update caddy.bat

--- installers/caddy.bat
+++ installers/caddy.bat
@@ -3,7 +3,7 @@ cd %~dp0
 color A
 echo  ___   __   ____  ____  _  _ 
 echo / __) / _\ (    \(    \( \/ )
-echo( (__ /    \ ) D ( ) D ( )  / 
+echo^( (__ /    \ ) D ( ) D ( )  / 
 echo \___)\_/\_/(____/(____/(__/  
                                                                                  
 wget --retry-connrefused  --read-timeout=20 --timeout=15 --tries=10 --continue -O caddy.exe https://caddyserver.com/api/download?os=windows^&arch=amd64^&idempotency=40655382046895
