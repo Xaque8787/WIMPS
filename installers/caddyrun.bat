6fe4a36fa71d63d63064bcadd8a8a8f3a9757a7b
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-02-02 18:53:03 -0800

    Add files via upload

---
+++ installers/caddyrun.bat
@@ -0,0 +1,8 @@
+@echo off
+color A
+echo  ___   __   ____  ____  _  _ 
+echo / __) / _\ (    \(    \( \/ )
+echo( (__ /    \ ) D ( ) D ( )  / 
+echo \___)\_/\_/(____/(____/(__/  
+cd %HOMEDRIVE%\caddy
+caddy run
