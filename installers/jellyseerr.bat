15df5a6ab73223e6b164529f3ed261b932d68d02
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-02-04 14:48:11 -0800

    Update jellyseerr.bat

--- installers/jellyseerr.bat
+++ installers/jellyseerr.bat
@@ -3,18 +3,7 @@ color 5
 echo _(  )(  __)(  )  (  )  ( \/ )/ ___)(  __)(  __)(  _ \(  _ \
 echo/ \) \ ) _) / (_/\/ (_/\ )  / \___ \ ) _)  ) _)  )   / )   /
 echo\____/(____)\____/\____/(__/  (____/(____)(____)(__\_)(__\_)                                                                                 
-set "PATH=%APPDATA%\npm;%~dp0;%PATH%"
 
-setlocal enabledelayedexpansion
-pushd "%~dp0"
-
-popd
-endlocal
-if "%CD%\"=="%~dp0" cd /d "%HOMEDRIVE%%HOMEPATH%"
-
-
-call npm install -g npm
-call npm install -g win-node-env
 cd %~dp0 
 wget https://github.com/Fallenbagel/jellyseerr/archive/refs/heads/main.zip
 7z x "main.zip" "-o%HOMEDRIVE%"
