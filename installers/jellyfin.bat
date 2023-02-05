c1471a09feaf856de5876f025b246f464db9bd88
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-02-04 17:03:46 -0800

    Update jellyfin.bat

--- installers/jellyfin.bat
+++ installers/jellyfin.bat
@@ -6,6 +6,6 @@ echo/ \) \ ) _) / (_/\/ (_/\ )  /  ) _)  )( /    /
 echo\____/(____)\____/\____/(__/  (__)  (__)\_)__)                                                                                 
 
 wget -O jellyfin.exe https://github.com/jellyfin/jellyfin-server-windows/releases/download/10.8.9/jellyfin_10.8.9_windows-x64.exe
-jellyfin.exe /S
+jellyfin.exe
 timeout 3
 del jellyfin.exe
