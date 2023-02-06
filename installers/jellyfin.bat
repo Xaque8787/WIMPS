15a70f4bc1fcf501169416b4af31873c5aa53d61
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-02-05 22:49:49 -0800

    Update jellyfin.bat

--- installers/jellyfin.bat
+++ installers/jellyfin.bat
@@ -16,5 +16,5 @@ cd "%PROGRAMFILES%\Jellyfin\Server"
 call nssm.exe install JellyfinServer "%PROGRAMFILES%\Jellyfin\Server\jellyfin.exe" --service --datadir "%PROGRAMDATA%\Jellyfin"
 call nssm.exe start JellyfinServer
 cd %~dp0
-del jellyfin_10.8.9.zip
+rmdir /s /q "%~dp0jellyfin_10.8.9.zip"
 timeout 4
