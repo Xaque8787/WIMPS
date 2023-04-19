12f20c7bf4b2261e9283184c22b9ec5fc19cd218
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-04-18 21:10:18 -0700

    Update protonvpn.bat

--- installers/protonvpn.bat
+++ installers/protonvpn.bat
@@ -5,7 +5,7 @@ echo(  _ \(  _ \ /  \(_  _)/  \ (  ( \  / )( \(  _ \(  ( \
 echo ) __/ )   /(  O ) )( (  O )/    /  \ \/ / ) __//    /
 echo(__)  (__\_) \__/ (__) \__/ \_)__)   \__/ (__)  \_)__)                                                                                 
 
-wget https://github.com/ProtonVPN/win-app/releases/download/2.3.1/ProtonVPN_win_v2.3.1.exe
-ProtonVPN_win_v2.3.1.exe /quiet
+wget https://github.com/ProtonVPN/win-app/releases/download/2.4.1/ProtonVPN_win_v2.4.1.exe
+ProtonVPN_win_v2.4.1.exe /quiet
 timeout 3
-del ProtonVPN_win_v2.3.1.exe
+del ProtonVPN_win_v2.4.1.exe
