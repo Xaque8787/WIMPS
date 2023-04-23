1971da3273397f8417ac8a43e2fab5c0c73557b2
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-04-23 12:32:59 -0700

    Update sonarr.bat

--- installers/sonarr.bat
+++ installers/sonarr.bat
@@ -5,7 +5,7 @@ echo/ ___) /  \ (  ( \ / _\ (  _ \(  _ \
 echo\___ \(  O )/    //    \ )   / )   /
 echo(____/ \__/ \_)__)\_/\_/(__\_)(__\_)                                                                                 
 
-wget -O sonarr.exe https://download.sonarr.tv/v3/main/3.0.9.1549/Sonarr.main.3.0.9.1549.windows.exe
+wget -O sonarr.exe https://download.sonarr.tv/v3/main/3.0.10.1567/Sonarr.main.3.0.10.1567.windows.exe
 sonarr.exe /VERYSILENT /SUPPRESSMSGBOXES /LOADINF="sonarrinst" /LOG /NORESTART /LANG=en
 timeout 3
 del sonarr.exe
