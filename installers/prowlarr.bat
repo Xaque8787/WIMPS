8dd4e11e38acda5af382c3a611e8d3c39df3d975
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-04-23 12:34:47 -0700

    Update prowlarr.bat

--- installers/prowlarr.bat
+++ installers/prowlarr.bat
@@ -5,7 +5,7 @@ echo(  _ \(  _ \ /  \ / )( \(  )   / _\ (  _ \(  _ \
 echo ) __/ )   /(  O )\ /\ // (_/\/    \ )   / )   /
 echo(__)  (__\_) \__/ (_/\_)\____/\_/\_/(__\_)(__\_)                                                                                 
 
-wget -O prowlarr.exe https://github.com/Prowlarr/Prowlarr/releases/download/v1.0.0.2171/Prowlarr.develop.1.0.0.2171.windows-core-x64-installer.exe
+wget -O prowlarr.exe https://github.com/Prowlarr/Prowlarr/releases/download/v1.3.2.3006/Prowlarr.master.1.3.2.3006.windows-core-x64-installer.exe
 prowlarr.exe /VERYSILENT /SUPPRESSMSGBOXES /LOADINF="prowlarrinst" /LOG /NORESTART /LANG=en
 timeout 3
 del prowlarr.exe
