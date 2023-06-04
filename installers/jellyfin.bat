7e2f19b918ba807cc9d7f74f5527f208c70d43c9
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-06-03 20:05:49 -0700

    Update jellyfin.bat

--- installers/jellyfin.bat
+++ installers/jellyfin.bat
@@ -5,7 +5,7 @@ echo _(  )(  __)(  )  (  )  ( \/ )(  __)(  )(  ( \
 echo/ \) \ ) _) / (_/\/ (_/\ )  /  ) _)  )( /    /
 echo\____/(____)\____/\____/(__/  (__)  (__)\_)__)                                                                                 
 
-wget https://repo.jellyfin.org/releases/server/windows/stable/combined/jellyfin_10.8.10.zip
+wget --retry-connrefused --read-timeout=20 --timeout=15 --tries=10 --continue --no-check-certificate https://repo.jellyfin.org/releases/server/windows/stable/combined/jellyfin_10.8.10.zip
 7z x "jellyfin_10.8.9.zip" -o%CD%
 mkdir "%PROGRAMFILES%\Jellyfin"
 mkdir "%PROGRAMFILES%\Jellyfin\Server"
