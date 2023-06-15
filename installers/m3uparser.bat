3b0fd2e0b44334454edc9c27df0ebfe34172f790
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-06-14 19:26:22 -0700

    Update m3uparser.bat

--- installers/m3uparser.bat
+++ installers/m3uparser.bat
@@ -1,4 +1,5 @@
 @echo off
+cls
 cd %~dp0
 color C
 echo( \/ )( __ \/ )( \  (  _ \ / _\ (  _ \/ ___)(  __)(  _ \
@@ -9,3 +10,4 @@ echo\_)(_/(____/\____/  (__)  \_/\_/(__\_)(____/(____)(__\_)
 mkdir "%HOMEDRIVE%\m3uparser"
 move "%~dp0\parser.py" "%HOMEDRIVE%\m3uparser"
 timeout 3
+exit /b
