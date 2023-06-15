e8cd8f09a6e870b48ab6c380dec6ff147df40354
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-06-14 19:44:48 -0700

    Create threadfin.bat

---
+++ installers/threadfin.bat
@@ -0,0 +1,17 @@
+@echo off
+cls
+color E
+echo (_  _)/ )( \(  _ \(  __) / _\ (    \(  __)(  )(  ( \
+echo   )(  ) __ ( )   / ) _) /    \ ) D ( ) _)  )( /    /
+echo  (__) \_)(_/(__\_)(____)\_/\_/(____/(__)  (__)\_)__)                                                                                 
+
+cd %~dp0 
+wget https://github.com/Threadfin/Threadfin/archive/refs/heads/main.zip
+7z x "main.zip" "-o%HOMEPATH%"
+cd %HOMEPATH%\Threadfin-main
+go mod tidy && go mod vendor
+go build threadfin.go
+cd %~dp0
+timeout 3
+del main.zip
+exit /b
