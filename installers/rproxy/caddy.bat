e4e109788ae89faf67a7a96f8a4838a1cdd64a58
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-06-19 13:11:49 -0700

    Update caddy.bat

--- installers/rproxy/caddy.bat
+++ installers/rproxy/caddy.bat
@@ -8,8 +8,8 @@ echo( (__ /    \ ) D ( ) D ( )  /
 echo \___)\_/\_/(____/(____/(__/  
                                                                                  
 wget --retry-connrefused --read-timeout=20 --timeout=15 --tries=10 --continue -O caddy.exe https://caddyserver.com/api/download?os=windows^&arch=amd64^&idempotency=40655382046895
-mkdir "%HOMEDRIVE%\caddy"
-move "caddy.exe" "%HOMEDRIVE%\caddy"
-move "caddyrun.bat" "%HOMEDRIVE%\caddy"
+mkdir "%HOMEPATH%\caddy"
+move "caddy.exe" "%HOMEPATH%\caddy"
+move "caddyrun.bat" "%HOMEPATH%\caddy"
 timeout 3
 exit /b
