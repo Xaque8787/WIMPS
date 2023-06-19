259f5e0620a26dc47890ec6be664adf7bea01e06
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-06-19 12:50:56 -0700

    Update jfa.bat

--- installers/arr_apps/jfa.bat
+++ installers/arr_apps/jfa.bat
@@ -7,8 +7,8 @@ echo/ \) \ ) _)/    \(___)( (_ \(  O )
 echo\____/(__) \_/\_/      \___/ \__/                                                                                  
 
 wget -O jfa.zip https://github.com/hrfee/jfa-go/releases/download/v0.4.0/jfa-go_0.4.0_TrayIcon_Windows_x86_64.zip
-mkdir "%HOMEDRIVE%\jfa"
-7z x "jfa.zip" -o%HOMEDRIVE%\jfa
+mkdir "%HOMEPATH%\jfa"
+7z x "jfa.zip" -o%HOMEPATH%\jfa
 timeout 3
 del jfa.zip
 exit /b
