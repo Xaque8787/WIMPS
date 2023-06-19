3e5f155b9ef68e520df0eb0efb36ab26e7cb56cd
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-06-19 12:52:36 -0700

    Update jsinst.bat

--- installers/arr_apps/jsinst.bat
+++ installers/arr_apps/jsinst.bat
@@ -5,9 +5,9 @@ echo _(  )(  __)(  )  (  )  ( \/ )/ ___)(  __)(  __)(  _ \(  _ \
 echo/ \) \ ) _) / (_/\/ (_/\ )  / \___ \ ) _)  ) _)  )   / )   /
 echo\____/(____)\____/\____/(__/  (____/(____)(____)(__\_)(__\_)                                                                                 
 
-cd C:\jellyseerr-main
+cd %HOMEPATH%\jellyseerr-main
 call "%PROGRAMFILES%\nodejs\nodevars.bat"
-cd C:\jellyseerr-main
+cd %HOMEPATH%\jellyseerr-main
 call "C:\Program Files (x86)\Yarn\bin\yarn.bat" install
 call "C:\Program Files (x86)\Yarn\bin\yarn.bat" run build
 rem yarn install && yarn run build
