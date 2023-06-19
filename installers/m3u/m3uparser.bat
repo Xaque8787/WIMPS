7391bfe4b98266225b96679f61e339f5049f9ba3
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-06-19 13:36:30 -0700

    Update m3uparser.bat

--- installers/m3u/m3uparser.bat
+++ installers/m3u/m3uparser.bat
@@ -9,5 +9,7 @@ echo\_)(_/(____/\____/  (__)  \_/\_/(__\_)(____/(____)(__\_)
 
 mkdir "%HOMEPATH%\m3uparser"
 move "%~dp0\parser.py" "%HOMEPATH%\m3uparser"
+move "%~dp0\moviemover.py" "%HOMEPATH%\m3uparser"
+move "%~dp0\tvshowmover.py" "%HOMEPATH%\m3uparser"
 timeout 3
 exit /b
