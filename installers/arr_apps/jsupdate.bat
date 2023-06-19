38fde197f630f41f7a36a732665fdee540657a13
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-06-19 13:02:53 -0700

    Update jsupdate.bat

--- installers/arr_apps/jsupdate.bat
+++ installers/arr_apps/jsupdate.bat
@@ -5,16 +5,16 @@ echo _(  )(  __)(  )  (  )  ( \/ )/ ___)(  __)(  __)(  _ \(  _ \
 echo/ \) \ ) _) / (_/\/ (_/\ )  / \___ \ ) _)  ) _)  )   / )   /
 echo\____/(____)\____/\____/(__/  (____/(____)(____)(__\_)(__\_)                                                                                 
 
-cd C:\jellyseerr-main
-move /y config C:\
-cd C:\
+cd %HOMEPATH%\jellyseerr-main
+move /y config %HOMEPATH%
+cd %HOMEPATH%
 wget https://github.com/Fallenbagel/jellyseerr/archive/refs/heads/main.zip
-rmdir /s /q C:\jellyseerr-main
-7z x "main.zip" -o%HOMEDRIVE%
-cd %HOMEDRIVE%/jellyseerr-main
+rmdir /s /q %HOMEPATH%\jellyseerr-main
+7z x "main.zip" -o%HOMEPATH%
+cd %HOMEPATH%/jellyseerr-main
 yarn install && yarn run build
-cd C:\
-move /y config C:\jellyseerr-main
+cd %HOMEPATH%
+move /y config %HOMEPATH%\jellyseerr-main
 
 
 timeout 3
