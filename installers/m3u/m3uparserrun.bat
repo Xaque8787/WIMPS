45e91ac74b609798f0814a3aa2b1db1eb5ad7fb6
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-06-20 12:50:11 -0700

    Update m3uparserrun.bat

--- installers/m3u/m3uparserrun.bat
+++ installers/m3u/m3uparserrun.bat
@@ -9,21 +9,12 @@ echo.
 cd %~dp0
 set /p destination=<destination.cfg
 set /p vodurl=<vodurl.cfg
-if "%destination%" == "" (
-    echo Destination folder and VOD URL not set.
-    echo Do you want to set them now?
-    echo 1. Set Destination
-    echo 2. Exit
-    choice /c 12 /n /m "Enter your selection (1 or 2):"
-    if errorlevel 2 exit
-) else (
-    start "" /wait /b cmd.exe /c %HOMEPATH%\m3uparser\m3uinput.bat
-)
+
 wget -O m3u_file.m3u %vodurl%
-"%PROGRAMFILES%\Python\Python311\python.exe" "%HOMEPATH%\m3uparser\parser.py"
+"%PROGRAMFILES%\Python311\python.exe" %HOMEPATH%\m3uparser\parser.py
 timeout 3
-"%PROGRAMFILES%\Python\Python311\python.exe" "%HOMEPATH%\m3uparser\moviemover.py"
-"%PROGRAMFILES%\Python\Python311\python.exe" "%HOMEPATH%\m3uparser\tvshowmover.py"
+"%PROGRAMFILES%\Python311\python.exe" %HOMEPATH%\m3uparser\moviemover.py
+"%PROGRAMFILES%\Python311\python.exe" %HOMEPATH%\m3uparser\tvshowmover.py
 cd %~dp0
 rmdir /s /q "%HOMEPATH%\m3uparser\Movie VOD"
 rmdir /s /q "%HOMEPATH%\m3uparser\TV VOD"
