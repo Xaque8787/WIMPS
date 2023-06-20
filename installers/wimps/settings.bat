eb964236b9d4a5240131eed48c009405d5f69cc8
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-06-20 10:14:39 -0700

    Update settings.bat

--- installers/wimps/settings.bat
+++ installers/wimps/settings.bat
@@ -10,44 +10,40 @@ cd %~dp0
 setlocal enabledelayedexpansion
 
 for /f "tokens=1,2 delims==" %%a in (mainlist.cfg) do (
-set /A i+=1
-set "appname[!i!]=%%a"
-set "appval[!i!]=%%b"
+    set /A i+=1
+    set "appname[!i!]=%%a"
+    set "appval[!i!]=%%b"
 )
 
 set "checkApp1=caddy"
 set "checkApp2=m3uparser"
 
 for /L %%i in (1,1,!i!) do (
-if "!appname[%%i]!" == "%checkApp1%" (
-if "!appval[%%i]!" == "false" (
-
-choice /C YN /M "Caddy requires information to set up reverse proxy, enter this info now?"
-if errorlevel 2 (
-goto m3u
-) else if errorlevel 1 (
-start "" /wait /b cmd.exe /c "..\rproxy\rproxy.bat"
-goto m3u
-)
-)
-)
-:m3u
-if "!appname[%%i]!" == "%checkApp2%" (
-    if "!appval[%%i]!" == "false" (
-        
-
-        choice /C YN /M "m3uparser requires a destination folder for VOD media folders, and your m3u VOD url. Enter those now?"
-
-        if errorlevel 2 (
-            goto end
-        ) else if errorlevel 1 (
-            start "" /wait /b cmd.exe /c "..\m3u\m3uinput.bat"
+    if "!appname[%%i]!" == "%checkApp1%" (
+        if "!appval[%%i]!" == "false" (
+            choice /C YN /M "Caddy requires information to set up reverse proxy, enter this info now?"
+            if errorlevel 2 (
+                goto m3u
+            ) else if errorlevel 1 (
+                start "" /wait /b cmd.exe /c "..\rproxy\rproxy.bat"
+                goto m3u
+            )
+        )
+    )
+    
+    :m3u
+    if "!appname[%%i]!" == "%checkApp2%" (
+        if "!appval[%%i]!" == "false" (
+            choice /C YN /M "m3uparser requires a destination folder for VOD media folders, and your m3u VOD url. Enter those now?"
+            if errorlevel 2 (
+                goto end
+            ) else if errorlevel 1 (
+                start "" /wait /b cmd.exe /c "..\m3u\m3uinput.bat"
+            )
         )
     )
 )
-)
 
 :end
-
 endlocal
 
