eca4652cbab4c26f74a3c270f3412005b8d519fd
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-06-20 10:09:29 -0700

    Update jellyseerr.bat

--- installers/arr_apps/jellyseerr.bat
+++ installers/arr_apps/jellyseerr.bat
@@ -5,10 +5,11 @@ echo _(  )(  __)(  )  (  )  ( \/ )/ ___)(  __)(  __)(  _ \(  _ \
 echo/ \) \ ) _) / (_/\/ (_/\ )  / \___ \ ) _)  ) _)  )   / )   /
 echo\____/(____)\____/\____/(__/  (____/(____)(____)(__\_)(__\_)                                                                                 
 
-cd %~dp0 
+cd %~dp0
 wget https://github.com/Fallenbagel/jellyseerr/archive/refs/heads/main.zip
 7z x "main.zip" "-o%HOMEPATH%"
 cd %SystemRoot%\system32
+setx path "%path%;C:\Program Files (x86)\Yarn\bin"
 start jsinst.bat
 
 cd %~dp0
