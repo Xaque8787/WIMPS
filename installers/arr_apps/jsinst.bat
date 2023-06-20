a6c88dd9ec2d28af4b72257464e5e17c33b42793
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-06-20 10:08:21 -0700

    Update jsinst.bat

--- installers/arr_apps/jsinst.bat
+++ installers/arr_apps/jsinst.bat
@@ -8,9 +8,9 @@ echo\____/(____)\____/\____/(__/  (____/(____)(____)(__\_)(__\_)
 cd %HOMEPATH%\jellyseerr-main
 call "%PROGRAMFILES%\nodejs\nodevars.bat"
 cd %HOMEPATH%\jellyseerr-main
-call "C:\Program Files (x86)\Yarn\bin\yarn.bat" install
-call "C:\Program Files (x86)\Yarn\bin\yarn.bat" run build
-rem yarn install && yarn run build
+rem call "C:\Program Files (x86)\Yarn\bin\yarn.bat" install
+rem "C:\Program Files (x86)\Yarn\bin\yarn.bat" run build
+yarn install && yarn run build
 
 echo Run jellyseerr with 'jsrun' in command prompt.
 timeout 15
