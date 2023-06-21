eaa524f4b264fcaa7adfe114b30ba8004678d4cc
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-06-20 22:36:50 -0700

    Update protonvpn.bat

--- installers/vpn/protonvpn.bat
+++ installers/vpn/protonvpn.bat
@@ -6,8 +6,5 @@ echo(  _ \(  _ \ /  \(_  _)/  \ (  ( \  / )( \(  _ \(  ( \
 echo ) __/ )   /(  O ) )( (  O )/    /  \ \/ / ) __//    /
 echo(__)  (__\_) \__/ (__) \__/ \_)__)   \__/ (__)  \_)__)                                                                                 
 
-wget https://github.com/ProtonVPN/win-app/releases/download/3.0.5/ProtonVPN_v3.0.5.exe
-ProtonVPN_win_v3.0.5 /quiet
-timeout 3
-del ProtonVPN_win_v3.0.5
+winget install -e --id ProtonTechnologies.ProtonVPN
 exit /b
