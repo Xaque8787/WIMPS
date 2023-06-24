5501290add4f15933057544cac72ee326d696fdf
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-06-23 22:06:46 -0700

    Update noteplus.bat

--- installers/tools/noteplus.bat
+++ installers/tools/noteplus.bat
@@ -6,7 +6,7 @@ echo(  ( \ /  \(_  _)(  __)(  _ \ / _\ (    \ ( )  ( )
 echo/    /(  O ) )(   ) _)  ) __//    \ ) D ((_ _)(_ _) 
 echo\_)__) \__/ (__) (____)(__)  \_/\_/(____/ (_)  (_) 
 
-wget -O noteplus.exe https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.4.8/npp.8.4.8.Installer.x64.exe
+wget -O noteplus.exe https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.5.4/npp.8.5.4.Installer.x64.exe
 noteplus.exe /S
 timeout 3
 del noteplus.exe
