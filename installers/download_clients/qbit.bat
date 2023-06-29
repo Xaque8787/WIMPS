bdd0eb75d2e146dac7a160b42bc9c17581152f2f
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-06-28 19:39:21 -0700

    Update qbit.bat

--- installers/download_clients/qbit.bat
+++ installers/download_clients/qbit.bat
@@ -6,7 +6,7 @@ echo /  \ (  _ \(  )(_  _)(_  _)/  \(  _ \(  _ \(  __)(  ( \(_  _)
 echo(  O ) ) _ ( )(   )(    )( (  O ))   / )   / ) _) /    /  )(  
 echo \__\)(____/(__) (__)  (__) \__/(__\_)(__\_)(____)\_)__) (__)                                                                                  
 
-wget -O qbit.exe https://sourceforge.net/projects/qbittorrent/files/qbittorrent-win32/qbittorrent-4.5.3/qbittorrent_4.5.3_lt20_qt5_x64_setup.exe/download
+wget -O qbit.exe https://sourceforge.net/projects/qbittorrent/files/qbittorrent-win32/qbittorrent-4.5.4/qbittorrent_4.5.4_lt20_qt5_x64_setup.exe/download
 qbit.exe /S
 timeout 3
 del qbit.exe
