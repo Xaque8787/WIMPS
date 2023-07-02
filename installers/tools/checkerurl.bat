fa384504941d842684be2c9464e888ff26a68588
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-07-01 21:36:20 -0700

    Update checkerurl.bat

--- installers/tools/checkerurl.bat
+++ installers/tools/checkerurl.bat
@@ -4,25 +4,55 @@ setlocal enabledelayedexpansion
 echo / )( \(  _ \(  )     / __)/ )( \(  __)/ __)(  / )
 echo ) \/ ( )   // (_/\  ( (__ ) __ ( ) _)( (__  )  ( 
 echo \____/(__\_)\____/   \___)\_)(_/(____)\___)(__\_)
-set count_exists=0
-set count_not_exists=0
 
-for /f "delims=" %%i in (urls.txt) do (
-  for /f "tokens=1,2 delims==" %%a in ("%%i") do (
-    set name=%%a
-    set url=%%b
-    wget --spider "!url!" 2>&1 | find /i "Remote file exists." > nul
-    if errorlevel 1 (
-      echo !name!: Remote file does not exist -- broken link!!!
-      set /a count_not_exists=count_not_exists+1
-    ) else (
-      echo !name!: Remote file exists.
-      set /a count_exists=count_exists+1
-    )
+set "URL_FILE=urls.txt"
+set "OK_LIST="
+set "FAILURE_LIST="
+
+for /f "usebackq tokens=1,* delims==" %%A in ("%URL_FILE%") do (
+  set "NAME=%%A"
+  set "LINE=%%B"
+
+  REM Extract the name and URL
+  for /f "delims=" %%C in ("!LINE!") do set "URL=%%C"
+
+  echo !NAME!
+  echo !URL!
+
+  REM Check the protocol of the URL
+  set "PROTOCOL=http"
+  echo !URL! | findstr /b /c:"https://" >nul
+  if not errorlevel 1 set "PROTOCOL=https"
+
+  REM Perform HTTP check
+  curl --head --silent --output NUL --fail --insecure !URL!
+  if errorlevel 1 (
+    echo !PROTOCOL! !NAME! - Failure
+    REM Add the line to the FAILURE_LIST
+    set "FAILURE_LIST=!FAILURE_LIST!!NAME! - Failure\n"
+  ) else (
+    echo !PROTOCOL! !NAME! - OK
+    REM Add the line to the OK_LIST
+    set "OK_LIST=!OK_LIST!!NAME!\n"
   )
+
+  echo.
 )
 
-echo Number of results with Remote file exists: %count_exists%
-echo Number of results with Remote file does not exist -- broken link!!!: %count_not_exists%
+REM Clear the screen
+cls
+
+REM Reprint lines with "OK" status
+echo - OK Results -
+echo(%OK_LIST:\n=&echo.%
+
+echo.
+
+REM Reprint lines with "Failure" status
+echo - Failure Results -
+echo(%FAILURE_LIST:\n=&echo.%
+
+endlocal
+pause
 
 pause
