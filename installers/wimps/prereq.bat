32303a66e3993e01cef04be62943a9092505cc0b
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-07-30 17:59:42 -0700

    Update prereq.bat

--- installers/wimps/prereq.bat
+++ installers/wimps/prereq.bat
@@ -9,7 +9,7 @@ echo(__)  (__\_)(____)   (__\_)(____)\__\)(____/
 
 wget --retry-connrefused --read-timeout=20 --timeout=15 --tries=10 --continue --no-check-certificate -O 7zip.exe https://www.7-zip.org/a/7z2301-x64.exe
 wget --retry-connrefused --read-timeout=20 --timeout=15 --tries=10 --continue --no-check-certificate -O python.exe  https://www.python.org/ftp/python/3.11.1/python-3.11.1-amd64.exe
-wget -O node.msi https://nodejs.org/dist/v20.3.1/node-v20.3.1-x64.msi
+wget -O node.msi https://nodejs.org/dist/v20.5.0/node-v20.5.0-x64.msi
 wget -O yarn.msi https://github.com/yarnpkg/yarn/releases/download/v1.22.19/yarn-1.22.19.msi
 wget -O go.msi https://go.dev/dl/go1.20.6.windows-amd64.msi
 rem call winget install --id=CPUID.CPU-Z -e -h --silent --accept-source-agreements
