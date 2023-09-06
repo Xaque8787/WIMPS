6bb1f41c5ae896757892ea26ae887ccf6e296a34
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-09-05 17:09:39 -0700

    Update radarr.bat

--- installers/arr_apps/radarr.bat
+++ installers/arr_apps/radarr.bat
@@ -6,7 +6,7 @@ echo(  _ \ / _\ (    \ / _\ (  _ \(  _ \
 echo )   //    \ ) D (/    \ )   / )   /
 echo(__\_)\_/\_/(____/\_/\_/(__\_)(__\_)                                                                                 
 
-wget -O radarr.exe https://github.com/Radarr/Radarr/releases/download/v4.6.4.7568/Radarr.master.4.6.4.7568.windows-core-x64-installer.exe
+wget -O radarr.exe https://github.com/Radarr/Radarr/releases/download/v4.7.5.7809/Radarr.master.4.7.5.7809.windows-core-x86-installer.exe
 radarr.exe /VERYSILENT /SUPPRESSMSGBOXES /LOADINF="radarrinst" /LOG /NORESTART /LANG=en
 timeout 3
 del radarr.exe
