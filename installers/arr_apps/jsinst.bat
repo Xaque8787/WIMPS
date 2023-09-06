25b5b546a7272cab64f1c4ed7a227b1a998bb366
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-09-05 18:02:48 -0700

    Update jsinst.bat

--- installers/arr_apps/jsinst.bat
+++ installers/arr_apps/jsinst.bat
@@ -8,8 +8,6 @@ echo\____/(____)\____/\____/(__/  (____/(____)(____)(__\_)(__\_)
 cd %HOMEPATH%\jellyseerr-main
 call "%PROGRAMFILES%\nodejs\nodevars.bat"
 cd %HOMEPATH%\jellyseerr-main
-rem call "C:\Program Files (x86)\Yarn\bin\yarn.bat" install
-rem "C:\Program Files (x86)\Yarn\bin\yarn.bat" run build
 yarn install && yarn add typeorm@0.3.11 && yarn run build
 
 echo Run jellyseerr with 'jsrun' in command prompt.
