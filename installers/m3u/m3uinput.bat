fc4017b428cc50c76e48847df4ab1c29fb837a53
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-09-23 14:06:26 -0700

    Update m3uinput.bat

--- installers/m3u/m3uinput.bat
+++ installers/m3u/m3uinput.bat
@@ -16,6 +16,14 @@ set /p vodurl=
 echo %destination%|find /v "" > destination.cfg
 echo %vodurl%|find /v "" > vodurl.cfg
 
+echo Creating Windows Task to run at 2am everday
+
+set TaskName=MyScheduledTask
+set ProgramPath=%HOMEPATH%\m3uparser\m3uparserrun.bat
+set StartTime=02:00
+
+schtasks /create /tn "%TaskName%" /sc daily /st %StartTime% /tr "%ProgramPath%" /ru System
+
 timeout 3
 start %homepath%\m3uparser\m3uparserrun.bat
 exit /b
