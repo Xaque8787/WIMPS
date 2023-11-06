d3308d2f0ed8fe5d9416a5c7f0ff58681b1cd6da
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2023-11-05 18:24:31 -0800

    Update unpackerr.bat

--- installers/arr_apps/unpackerr.bat
+++ installers/arr_apps/unpackerr.bat
@@ -6,3 +6,9 @@ echo  _  _  __ _  ____   __    ___  __ _  ____  ____  ____
 echo / )( \(  ( \(  _ \ / _\  / __)(  / )(  __)(  _ \(  _ \
 echo ) \/ (/    / ) __//    \( (__  )  (  ) _)  )   / )   /
 echo \____/\_)__)(__)  \_/\_/ \___)(__\_)(____)(__\_)(__\_)
+wget https://github.com/Unpackerr/unpackerr/releases/download/v0.12.0/unpackerr.amd64.exe.zip
+mkdir "%HOMEPATH%\unperr"
+7z x "unpackerr.amd64.exe.zip" -o%HOMEPATH%\unperr
+del unpackerr.amd64.exe.zip
+cd "%HOMEPATH%\unperr"
+copy "unpackerr.amd64.exe" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup"
